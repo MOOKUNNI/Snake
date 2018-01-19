aa32e671bf9e62b8eea1b6c8d7e2d05531cd0a49
Jason Hwang <JasonHwang@1607017-2.local>
2018-01-19 19:39:36 +0900

    comiting my second codee

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
 
 public class Main {
-
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int gameHeight = 500;
+		int gameWidth = 500;
+		int frameHeight = (int) dimension.getHeight();
+		int frameWidth = (int) dimension.getWidth();
+		int y = frameHeight/2 - gameHeight/2;
+		int x = frameWidth/2 - gameWidth/2;
+		JFrame jf = new JFrame();
+		GamePlay play = new GamePlay();
+		jf.add(play);
+		jf.setBounds(x,y,gameWidth,gameHeight);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setBackground(Color.PINK);
+		jf.setVisible(true);
 
 	}
 
